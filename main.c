9595fc79e3705298055a949c63a5326665bacfb3
Lee Cloe Kyunghyun <cloelee@D-10-18-131-204.dhcp4.washington.edu>
2018-04-24 17:11:47 -0700

    debug configuration

--- main.c
+++ main.c
@@ -33,77 +33,94 @@ void readFile(int fd) {
 
 int main (void)
 {
-        //open serial device
-        int fd;
-        fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
-        if (fd == -1)
-        {
-        perror("open_port: Unable to open /dev/ttyS0 - ");
-        }
-        else
-        printf("succeed to open ttyS0 port...\n");
-        fcntl(fd, F_SETFL, 0);
-        //Set baudrate to 9600
-        struct termios options;
-        tcgetattr(fd, &options);
-        cfsetispeed(&options, B9600);
-        cfsetospeed(&options, B9600);
-    
-        // Set the Charactor size
-    
-        options.c_cflag &= ~CSIZE; /* Mask the character size bits */
-        options.c_cflag |= CS8;    /* Select 8 data bits */
-    
-        // Set parity - No Parity (8N1)
-    
-        options.c_cflag &= ~PARENB;
-        options.c_cflag &= ~CSTOPB;
-        options.c_cflag &= ~CSIZE;
-        options.c_cflag |= CS8;
-    
-        options.c_cflag |= (CLOCAL | CREAD);
-    
-        if ( tcsetattr( fd, TCSANOW, &options ) == -1 )
-            printf ("Error with tcsetattr = %s\n", strerror ( errno ) );
-        else
-            printf ( "%s\n", "tcsetattr succeed" );
-    
-        printf("-> start reading data\n");
-        do {
-            unsigned char buffer[100];
-            ssize_t length = read(fd, buffer, sizeof(buffer) - 1);
-            if (length == -1)
-            {
-                printf("Error reading from serial port\n");
-                break;
-            }
-            else if (length == 0)
-            {
-                printf("No more data\n");
-                break;
-            }
-            else
-            {
-                buffer[length] = '\0';
-                printf("%s\n", buffer);
-            }
-        } while (1);
-       
-        // //Write text
-        // int n;
-        // n = write(fd, "Hello world!\r", 13);
-        // if (n < 0)
-        //         fputs("write() failed!\n", stderr);
- 
-        // n = write(fd, "Printed using the VoCore v1.0\r", 30);
-        // if (n < 0)
-        //         fputs("write() failed!\n", stderr);
- 
-        // n = write(fd, ":)\r", 3);
-        // if (n < 0)
-        //         fputs("write() failed!\n", stderr);
- 
-        //close connection
-        close(fd);
-        return printf("End\n");
+  printf("\n +----------------------------------+");
+  printf("\n |        Serial Port Write         |");
+  printf("\n +----------------------------------+");
+
+  //open serial device
+  int fd;
+  fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
+  if (fd == -1)
+  {
+  perror("open_port: Unable to open /dev/ttyS0 - ");
+  }
+  else
+  printf("succeed to open ttyS0 port...\n");
+  fcntl(fd, F_SETFL, 0);
+  //Set baudrate to 9600
+  struct termios options;
+  tcgetattr(fd, &options);
+  cfsetispeed(&options, B9600);
+  cfsetospeed(&options, B9600);
+
+  // Set parity - No Parity (8N1)
+
+  options.c_cflag &= ~PARENB;
+  options.c_cflag &= ~CSTOPB;
+  options.c_cflag &= ~CSIZE;
+  options.c_cflag |= CS8;
+
+  //Turn off hardware based flow control (RTS/CTS).
+  SerialPortSettings.c_cflag &= ~CRTSCTS;
+
+  // Turn on the receiver of the serial port (CREAD)
+  options.c_cflag |= CREAD | CLOCAL;
+
+  // Turn off software based flow control (XON/XOFF).
+  SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);
+
+  // Serial communications with outside devices
+  // NON Cannonical mode is recommended.
+  SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+
+  SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
+
+  /* Setting Time outs */
+  options.c_cc[VMIN]  = 10; /* Read 10 characters */
+  options.c_cc[VTIME] = 0;  /* Wait indefinitely   */
+
+  if ( tcsetattr( fd, TCSANOW, &options ) == -1 )
+      printf ("Error with tcsetattr = %s\n", strerror ( errno ) );
+  else
+      printf ( "%s\n", "tcsetattr succeed" );
+      printf("\n  BaudRate = 9600 \n  StopBits = 1 \n  Parity   = none");
+
+  printf("-> start reading data\n");
+  do {
+      unsigned char buffer[32];
+      ssize_t length = read(fd, &buffer, sizeof(buffer) - 1);
+      if (length == -1)
+      {
+          printf("Error reading from serial port\n");
+          break;
+      }
+      else if (length == 0)
+      {
+          printf("No more data\n");
+          break;
+      }
+      else
+      {
+          buffer[length] = '\0';
+          printf("%s\n", buffer);
+      }
+  } while (1);
+
+  // //Write text
+  // int n;
+  // n = write(fd, "Hello world!\r", 13);
+  // if (n < 0)
+  //         fputs("write() failed!\n", stderr);
+
+  // n = write(fd, "Printed using the VoCore v1.0\r", 30);
+  // if (n < 0)
+  //         fputs("write() failed!\n", stderr);
+
+  // n = write(fd, ":)\r", 3);
+  // if (n < 0)
+  //         fputs("write() failed!\n", stderr);
+
+  //close connection
+  close(fd);
+  return printf("End\n");
 }
